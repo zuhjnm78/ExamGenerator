322dd0063a4b43dba46c072ef32609e98f6a5739
oleghenkel <oleg.henkel75@gmail.com>
2023-08-22 19:51:52 +0200

    Курсовая работа 2-го курса базовая сложсложность

---
+++ src/test/java/ru/skypro/examgenerator/ExamGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.examgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
